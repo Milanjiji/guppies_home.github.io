316cccea6918d7247105e9996df576617b4ad7b3
milanjiji <milanjiji0987654321@gmail.com>
2020-11-14 11:09:58 +0530

    my guppies

---
+++ open.js
@@ -0,0 +1,8 @@
+function openfunction(){
+    document.getElementById('menu').style.width="50%"
+    
+  }
+  function closefunction(){
+    document.getElementById('menu').style.width="0%"
+    
+  }
